3d34614ea5aca479327d9fea3f140792f257b9a2
Jack.W <www.445630956@qq.com>
2019-09-26 21:53:10 +0800

    'first'

---
+++ src/main/java/com/fulldress/darkpurple/DarkpurpleApplication.java
@@ -0,0 +1,13 @@
+package com.fulldress.darkpurple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkpurpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkpurpleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fulldress/darkpurple/DarkpurpleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fulldress.darkpurple;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DarkpurpleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
